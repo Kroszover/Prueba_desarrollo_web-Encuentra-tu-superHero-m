e919b14d51817b0202c40101321f048a56df2b70
CamiloLavado <camilolavado@petalmail.com>
2022-03-04 12:17:35 -0300

    requerimientos terminados, falta mejorar el front

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,4 +1,6 @@
 /*
+.- 3. Una vez ingresado el número del héroe a buscar y después de realizar un click sobre el botón de búsqueda, se debe capturar y validar la información para evitar búsquedas que contengan algún texto diferente a números y mostrar la información dinámicamente mediante la librería jQuery y CanvasJS con un gráfico de pastel. Para lograr todo esto se debe: (8 Puntos)
+
 .- 3.1 Capturar la información ingresada mediante eventos del DOM con jQuery. (1 Punto) 
 
 .- 3.2 Implementar funciones para separar la captura de la información ingresada por el usuario con la consulta a la API. (1 Punto) 
@@ -32,6 +34,7 @@ $('#botonConsulta').click
     //.- 3.3 Comprobar la información ingresada por el usuario, la cual, solo debe ser un número. (0.5 Puntos)
     //Validamos que lo ingresado sea un número.
     if (validar(id)) {
+       // .- 3.2 Implementar funciones para separar la captura de la información ingresada por el usuario con la consulta a la API. (1 Punto) 
         //Ejecutamos la consulta a la api
       llamarApi(id);
         //Mostramos el contenido que habiamos ocultado previamente
@@ -41,6 +44,7 @@ $('#botonConsulta').click
 
 //Funciones Usadas.-
 
+//.- 3.7 Emplear la librería de gráficos CanvasJS, para mostrar dinámicamente información específica de cada superhéroe. (2 Puntos) 
 function revela_grafico(elemento) {
     //Creamos un arreglo que aparecerá en el grafico
     let estadisticas = [];
@@ -74,6 +78,8 @@ function revela_grafico(elemento) {
     chart.render();
 }
 
+//.- 3.8 Implementar estructuras condicionales para generar alertas cuando existan errores en la búsqueda. (0.5 Punto)*/
+//Funcion para validar que lo ingresado sea un numero
 function validar (input){
     if (input == ''){
         $("#error1").text("Debe ingresar un número");
@@ -86,14 +92,73 @@ function validar (input){
         return false;
     }
 }
+
+//.- 3.4 Consultar la API mediante AJAX con la sintaxis de jQuery. (1 Punto) 
 //Funcion para consultar o llamar a la api con el mentodo AJAX
 function llamarApi(id){
     //Ingresamos el token para poder realizar la consulta.
     const token = "4905856019427443";
     //Invocamos al metodo AJAX
     $.ajax({
-        
-    })
+        type: "GET",
+            url:        "https://www.superheroapi.com/api.php/"+token+"/"+id ,
+            success:    function(data) {
+                
+                //se verifica que no tenga error
+                if (data.response=="error") {
+                    alert(data.error);
+                } else {
+                    let nombre      = data.name;
+                    let conexiones  = data.connections['group-affiliation'];
+
+
+                    //se leen variables y crea el parrafo
+                    let parrafo1    = `
+                        <h5>Nombre: ${nombre}</h5>
+                        <p>conexiones: ${conexiones}</p>
+                    `;
+
+                    //se inserta al html
+                    $("#infoHero1").html(parrafo1);
+
+                    let publicado           = data.biography.publisher;
+                    let ocupacion           = data.work.occupation;
+                    let primera_aparicion   = data.biography["first-appearance"];
+                    let altura              = data.appearance.height['0']+ " - "+data.appearance.height['1'];
+                    let peso                = data.appearance.weight['0']+ " - "+data.appearance.weight['1'];
+                    let alianzas            = "";
+                    
+
+                    //.- 3.6 Utilizar ciclos y métodos para arreglos u objetos que permitan recorrer, ordenar y mostrar la información. (1 Punto) 
+                    //se recorre el objeto para obtener alianzas
+                    for (const property in data.biography.aliases) {
+                        alianzas = alianzas+` ${data.biography.aliases[property]}`;
+                    }
+                    
+                    //se crea el segundo parrafo con las variables
+                    let parrafo2    = `
+                    <small>
+                        <p>publicado por: ${publicado}</p>
+                        <p>ocupacion: ${ocupacion}</p>
+                        <p>primera_aparicion: ${primera_aparicion}</p>
+                        <p>altura: ${altura}</p>
+                        <p>peso: ${peso}</p>
+                        <p>alianzas: ${alianzas}</p>
+                    </small>
+                    `;
+                    //se inserta el parrrafo
+                    $("#infoHero2").html(parrafo2);
+                    //se inserta la imagen
+                    $("#imagenSuper").attr("src",data.image.url);
+
+                    //se muestra el grafico.
+                    revela_grafico(data);
+                }
+                
+            },
+            error: ()=> alert("Conexión no disponible")
+
+    });
 
 }
 
